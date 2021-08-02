c31a221fdca8ab28982369b774529a3a7bc89f2d
ospreystudio <64425019+ospreystudio@users.noreply.github.com>
2021-08-02 13:14:00 +0400

    layout complite

---
+++ src/views/Create.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="row">
+<div class="col s6 offset-s3">
+  <h1>Create task</h1>
+  <form @submit.prevent="submitHandler">
+    <div class="input-field">
+      <input  id="title" type="text" class="validate" v-model="title" required>
+      <label for="title">Title</label>
+      <span class="helper-text" data-error="Title is required" ></span>
+    </div>
+    <div class="chips" ref="chips"></div>
+    <div class="input-field">
+      <textarea id="description" class="materialize-textarea" v-model="description"></textarea>
+      <label for="description">Textarea</label>
+      <span class="character-counter" style="font-size: 12px; float: right">{{description.length}}/2048</span>
+    </div>
+    <input type="text" ref="datepicker">
+    <button class="btn" type="submit"> Create task </button>
+  </form>
+</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'create',
+  data: () => ({
+    description: '',
+    title: '',
+    chips: null,
+    date: null
+  }),
+  mounted() {
+    this.chips = M.Chips.init(this.$refs.chips, {
+      placeholder: 'Task tags'
+    })
+    this.date = M.Datepicker.init(this.$refs.datepicker, {
+        format: 'dd.mm.yyyy',
+        defaultDate: new Date(),
+        setDefaultDate: true
+    })
+    },
+  methods: {
+    submitHandler() {
+      const task = {
+        title: this.title,
+        description: this.description,
+        id: Date.now(),
+        status: 'active',
+        tags: this.chips.chipsData,
+        date: this.date.date
+      }
+      console.log(task)
+    }
+  },
+    destroyed() {
+      if (this.date && this.date.destroy) {
+        this.date.destroy()
+      }
+      if (this.chips && this.chips.destroy) {
+        this.chips.destroy()
+      }
+  }
+}
+</script>
