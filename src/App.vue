7d43efb8cbd820bad43006428d57cb4f011a9523
ospreystudio <64425019+ospreystudio@users.noreply.github.com>
2021-08-02 11:33:39 +0400

    components is ready

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Navbar />
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
+
+<script>
+import Navbar from "./components/Navbar";
+export  default  {
+  components: {Navbar}
+}
+</script>
 <style lang="scss">
  @import "~materialize-css";
 </style>
